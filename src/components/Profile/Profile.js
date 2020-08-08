430a1e8c49ef71b6f736af0f02e90c0e5c2f9041
Gerardo Castillo <gerrcass@gmail.com>
2020-08-08 15:02:48 -0400

    Passing down the user object to the Profile component so that user information can be dinamically displayed there.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Profile.css";
 
-const Profile = ({ isProfileOpen, toggleModal }) => {
+const Profile = ({ isProfileOpen, toggleModal, user }) => {
   return (
     <div className="profile-modal">
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
@@ -11,16 +11,16 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
             className="h3 w3 dib"
             alt="avatar"
           />
-          <h1>John Doe</h1>
-          <h4>Images Submited: 5</h4>
-          <p>Member since: January</p>
+          <h1>{user.name}</h1>
+          <h4>{`Images Submited: ${user.entries}`}</h4>
+          <p>{`Member since: ${new Date(user.joined).toLocaleDateString()}`}</p>
           <hr />
           <label className="mt2 fw6" htmlFor="user-name">
             Name:
           </label>
           <input
             className="pa2 ba w-100"
-            placeholder="John"
+            placeholder={user.name}
             type="text"
             name="user-name"
             id="name"
@@ -30,7 +30,7 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
           </label>
           <input
             className="pa2 ba w-100"
-            placeholder="52"
+            placeholder={user.age}
             type="text"
             name="user-age"
             id="age"
@@ -40,7 +40,7 @@ const Profile = ({ isProfileOpen, toggleModal }) => {
           </label>
           <input
             className="pa2 ba w-100"
-            placeholder="Dragon"
+            placeholder={user.pet}
             type="text"
             name="user-pet"
             id="pet"
